32bd9614f28b6919166739b3a4be3f219b2c9427
JamilErasmo <jamilerasmoq@gmail.com>
2023-06-05 22:38:41 -0500

    final

--- configuracion.py
+++ configuracion.py
@@ -1,6 +1,6 @@
 # Mysql
 # para el uso de este dialecto en SqlAlchemy
 # instalar "pip install PyMySQL"
-cadena_base_datos = 'mysql+pymysql://jamil:admin@localhost/final1bimaa22'
+#cadena_base_datos = 'mysql+pymysql://jamil:admin@localhost/final1bimaa22'
 
-#cadena_base_datos = 'sqlite:///Prueba01.db' 
+cadena_base_datos = 'sqlite:///final1bimaa22.db' 
